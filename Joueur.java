39b215342883c4dc0c9551463a9c31cc90ed3157
chineseman4 <chineseman4@users.noreply.github.com>
2015-01-09 09:55:20 +0100

    Create Joueur.java

---
+++ Joueur.java
@@ -0,0 +1,35 @@
+public class Joueur {
+
+	private String joueur;
+	private int score;
+	
+	public Joueur(String j, int s){
+		this.setJoueur(j);
+		this.setScore(s);
+	}
+	
+	public Joueur(){
+		setJoueur("Joueur 1");
+		setScore(0);
+	}
+
+	public String getJoueur() {
+		return joueur;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public void setJoueur(String joueur ) {
+		this.joueur = joueur;
+	}
+	
+	public String toString() {
+		return this.joueur + ", " + this.score +" points.";
+	}
+}
